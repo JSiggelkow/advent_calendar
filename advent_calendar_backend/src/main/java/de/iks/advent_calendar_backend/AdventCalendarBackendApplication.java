abbbb15207558e53ff2ae5a43f4631f713b4da33
JSiggelkow <152495705+JSiggelkow@users.noreply.github.com>
2023-12-05 09:11:30 +0100

    advent calendar backend

---
+++ advent_calendar_backend/src/main/java/de/iks/advent_calendar_backend/AdventCalendarBackendApplication.java
@@ -0,0 +1,13 @@
+package de.iks.advent_calendar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventCalendarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventCalendarBackendApplication.class, args);
+	}
+
+}
---
+++ advent_calendar_backend/src/test/java/de/iks/advent_calendar_backend/AdventCalendarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.iks.advent_calendar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventCalendarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
