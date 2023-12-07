5c5a27f815378128e7b9ea4a87f4bfe0f20191f9
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2023-12-07 13:46:08 +0100

    added advent calendar doors + numbers

--- advent_calendar_frontend/src/app/clendar-scaffold/door/door.component.ts
+++ advent_calendar_frontend/src/app/clendar-scaffold/door/door.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-door',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './door.component.scss'
 })
 export class DoorComponent {
-
+  @Input() doorNumber: number | undefined;
 }
--- advent_calendar_frontend/src/app/clendar-scaffold/scaffold/scaffold.component.ts
+++ advent_calendar_frontend/src/app/clendar-scaffold/scaffold/scaffold.component.ts
@@ -13,6 +13,14 @@ export class ScaffoldComponent {
   doors: number[] = [];
 
   constructor() {
-    this.doors = Array.from({length:24},(_, index) => index +1);
+    this.doors = this.shuffleArray(Array.from({ length: 24 }, (_, index) => index + 1));
+  }
+
+  private shuffleArray(array: number[]): number[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   }
 }
