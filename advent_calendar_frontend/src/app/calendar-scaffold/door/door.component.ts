76b5b569051c101de18e539d7f0e54b32c6cc1ab
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2023-12-11 17:13:49 +0100

    💫 Add shake animation for unauthorized access to door

--- advent_calendar_frontend/src/app/calendar-scaffold/door/door.component.ts
+++ advent_calendar_frontend/src/app/calendar-scaffold/door/door.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input} from '@angular/core';
-import {state, style, transition,animate, trigger} from '@angular/animations';
+import {Component, Input, OnInit} from '@angular/core';
+import {state, style, transition, animate, trigger} from '@angular/animations';
 import {Router, RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
@@ -28,22 +28,52 @@ import {Router, RouterLink, RouterOutlet} from "@angular/router";
         animate('0s')
       ]),
     ]),
+    trigger('shake', [
+      state('start', style({transform: 'translateX(0)'})),
+      state('end', style({transform: 'translateX(-5px)'})),
+      transition('start <=> end', [animate('100ms ease-in-out')]),
+    ]),
   ],
 })
-export class DoorComponent {
+export class DoorComponent implements OnInit {
   @Input() doorNumber: number | undefined;
+  @Input() openDoorDate: String | undefined;
   isOpen = false;
+  allowedToOpen = false;
+  shakeAnimationState = 'start';
 
   constructor(private router: Router) {
   }
 
+  ngOnInit(): void {
+    if (this.openDoorDate != null) {
+      this.allowedToOpen = Date.now() >= +this.openDoorDate;
+    }
+  }
+
   toggle() {
-    if (!this.isOpen) {
+    if (!this.isOpen && this.allowedToOpen) {
       this.isOpen = true;
 
       setTimeout(() => {
+        console.log(this.openDoorDate);
         this.router.navigate([this.doorNumber]);
       }, 600);
+    } else {
+      //start shake animation
+      this.startShake();
     }
+    return true;
   }
+
+  startShake() {
+    this.shakeAnimationState = 'start';
+    setTimeout(() => {
+      this.shakeAnimationState = 'end';
+      setTimeout(() => {
+        this.shakeAnimationState = 'start';
+      }, 100);
+    }, 0);
+  }
+
 }
