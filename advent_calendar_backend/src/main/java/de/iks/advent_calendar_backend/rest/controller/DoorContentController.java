fb05f966b6fc7a06ad3c62d0467908187d5b95cb
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2023-12-11 14:50:06 +0100

    🦺 get door content on correct date validation

--- advent_calendar_backend/src/main/java/de/iks/advent_calendar_backend/rest/controller/DoorContentController.java
+++ advent_calendar_backend/src/main/java/de/iks/advent_calendar_backend/rest/controller/DoorContentController.java
@@ -1,12 +1,15 @@
 package de.iks.advent_calendar_backend.rest.controller;
 
+import de.iks.advent_calendar_backend.entity.AdventCalendarDoor;
 import de.iks.advent_calendar_backend.entity.DoorContent;
+import de.iks.advent_calendar_backend.rest.service.AdventCalendarDoorService;
 import de.iks.advent_calendar_backend.rest.service.DoorContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -15,6 +18,9 @@ public class DoorContentController {
 
 	@Autowired
 	private DoorContentService doorContentService;
+
+	@Autowired
+	private AdventCalendarDoorService doorService;
 
 	@GetMapping
 	public List<DoorContent> getAllDoorContents() {
@@ -25,6 +31,19 @@ public class DoorContentController {
 	public ResponseEntity<DoorContent> getDoorContentById(@PathVariable int id) {
 		DoorContent doorContent = doorContentService.getDoorContentById(id);
 		return ResponseEntity.ok(doorContent);
+	}
+
+	@GetMapping("/validate/{id}")
+	public ResponseEntity<?> validateOpenDoorContent(@PathVariable int id) {
+		DoorContent doorContent = doorContentService.getDoorContentById(id);
+		int doorId = doorContent.getDoorId();
+		AdventCalendarDoor door = doorService.getDoorById(doorId);
+		Date currentDate = new Date();
+		if (door.getOpening_date().getTime() <= currentDate.getTime()) {
+			return ResponseEntity.ok(doorContent);
+		} else {
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Door cannot get opened " + currentDate.toString() + " | " + door.getOpening_date().toString());
+		}
 	}
 
 	@PostMapping
