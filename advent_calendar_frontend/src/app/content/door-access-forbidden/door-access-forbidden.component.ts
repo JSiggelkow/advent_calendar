e5bcc8caccf1370e04cbf5b8915462a43c47b922
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2023-12-11 16:32:31 +0100

    💄 UI for '/forbidden' route

--- advent_calendar_frontend/src/app/content/door-access-forbidden/door-access-forbidden.component.ts
+++ advent_calendar_frontend/src/app/content/door-access-forbidden/door-access-forbidden.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-door-access-forbidden',
@@ -8,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './door-access-forbidden.component.scss'
 })
 export class DoorAccessForbiddenComponent {
+
+  constructor(private router: Router) {
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClick(event: Event) {
+    const popupOverlay = document.querySelector('.popup-overlay');
+    const header = document.querySelector('#header');
+    if (
+      (popupOverlay && popupOverlay.contains(event.target as Node)) ||
+      (header && header.contains(event.target as Node))
+    ) {
+      return;
+    }
+
+    this.router.navigate(['/']);
+  }
+
+  goBack(): void {
+    this.router.navigate(['/'])
+  }
 
 }
