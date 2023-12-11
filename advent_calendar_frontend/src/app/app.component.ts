eb8cdec7d21f735ad2fbffd509992fbc33346d76
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2023-12-11 13:06:19 +0100

    ✨GET Door Numbers from db with RestApi

--- advent_calendar_frontend/src/app/app.component.ts
+++ advent_calendar_frontend/src/app/app.component.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./header/header/header.component";
 import {ScaffoldComponent} from "./calendar-scaffold/scaffold/scaffold.component";
-import {TestCrudComponent} from "./test-crud/test-crud.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, ScaffoldComponent, RouterLink, TestCrudComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, ScaffoldComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- advent_calendar_frontend/src/app/calendar-scaffold/scaffold/scaffold.component.ts
+++ advent_calendar_frontend/src/app/calendar-scaffold/scaffold/scaffold.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {DoorComponent} from "../door/door.component";
 import {NgForOf} from "@angular/common";
+import {AdventCalendarDoor, AdventCalendarDoorService} from "../../services/entities/AdventCalendarDoor.service";
 
 @Component({
   selector: 'app-scaffold',
@@ -10,17 +11,10 @@ import {NgForOf} from "@angular/common";
   styleUrl: './scaffold.component.scss'
 })
 export class ScaffoldComponent {
-  doors: number[] = [];
+  adventCalendarDoors: AdventCalendarDoor[] | undefined = [];
 
-  constructor() {
-    this.doors = this.shuffleArray(Array.from({ length: 24 }, (_, index) => index + 1));
-  }
-
-  private shuffleArray(array: number[]): number[] {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+  constructor(private adventCalendar: AdventCalendarDoorService) {
+    this.adventCalendar.getAll('http://localhost:8080/api/calendar-doors')
+      .subscribe(data => this.adventCalendarDoors = data);
   }
 }
