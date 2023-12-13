3e67d0d6fa1bc73da1fd24f90d9e91e4cca79ddd
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2023-12-13 15:31:31 +0100

    🛂 added AuthController

---
+++ advent_calendar_backend/src/main/java/de/iks/advent_calendar_backend/rest/controller/AuthController.java
@@ -0,0 +1,17 @@
+package de.iks.advent_calendar_backend.rest.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+	@PostMapping("/login")
+	public void login() {
+
+	}
+}
