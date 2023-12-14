bafd2275b8fecc5725b491339275f5abf90d8ee0
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2023-12-14 11:12:28 +0100

    🚧 UserPrincipal and token for later User JWT auth

---
+++ advent_calendar_backend/src/main/java/de/iks/advent_calendar_backend/security/UserPrincipalAuthenticationToken.java
@@ -0,0 +1,23 @@
+package de.iks.advent_calendar_backend.security;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+
+
+public class UserPrincipalAuthenticationToken extends AbstractAuthenticationToken {
+	private final UserPrincipal principal;
+	public UserPrincipalAuthenticationToken(UserPrincipal principal) {
+		super(principal.getAuthorities());
+		this.principal = principal;
+		setAuthenticated(true);
+	}
+
+	@Override
+	public Object getCredentials() {
+		return null;
+	}
+
+	@Override
+	public UserPrincipal getPrincipal() {
+		return principal;
+	}
+}
