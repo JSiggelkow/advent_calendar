610f8ba73702c5a99bbbdfa746a25d3cb7d81270
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2023-12-14 10:26:48 +0100

    🛂 get response data as JWT Token

--- advent_calendar_backend/src/main/java/de/iks/advent_calendar_backend/model/LoginRequest.java
+++ advent_calendar_backend/src/main/java/de/iks/advent_calendar_backend/model/LoginRequest.java
@@ -2,9 +2,10 @@ package de.iks.advent_calendar_backend.model;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
+@NoArgsConstructor
 public class LoginRequest {
 	private String username;
 	private String password;
