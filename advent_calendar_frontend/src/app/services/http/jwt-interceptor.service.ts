8eacad756c8fc7d88f0ac6306ba85338fb297f54
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2023-12-15 09:52:43 +0100

    🛂 add httpInterceptor for JWT Token header

--- advent_calendar_frontend/src/app/services/http/jwt-interceptor.service.ts
+++ advent_calendar_frontend/src/app/services/http/jwt-interceptor.service.ts
@@ -9,18 +9,16 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  intercept(
-    request: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem('JWT');
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = sessionStorage.getItem('JWT');
     if (token) {
+      console.log("kaka");
+      console.log(token);
       const authRequest = request.clone({
-        setHeaders: { Authorization: `Bearer ${token}` },
+        headers: request.headers.set('Authorization', `Bearer ${token}`)
       });
       return next.handle(authRequest);
     }
-
     return next.handle(request);
   }
 }
