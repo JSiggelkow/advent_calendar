181675c6ae24918ba19461cabf61e000f4a3c8a2
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2023-12-15 17:15:12 +0100

    🛂 (AuthService): Implement login and isLoggedIn methods
    Implemented the login method in AuthService to handle user authentication by making a request to the login API endpoint. If the authentication is successful, the access token is stored in the session storage, and the isLoggedInSubject BehaviorSubject is updated to true. If the authentication fails, an error message is logged, and isLoggedInSubject is updated to false.
    
    Also, added the isLoggedIn method to AuthService, which makes a request to a secured API endpoint to check if the user is logged in. If the request is successful, isLoggedInSubject is updated to true; otherwise, it is updated to false.
    
    These methods provide a way to manage user authentication status and can be used in various components and guards throughout the application.

---
+++ advent_calendar_frontend/src/app/services/auth.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable, tap} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {LoginService} from "./entities/Login.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrlLogin: string = "http://localhost:8080/api/auth/login";
+  private apiUrlSecured: string = "http://localhost:8080/api/auth/secured";
+  private isLoggedInSubject = new BehaviorSubject<boolean>(false);
+  public isLoggedIn$ = this.isLoggedInSubject.asObservable();
+
+  constructor(private http: HttpClient, private loginService: LoginService) {
+  }
+
+  login(credentials: { username: string; password: string }) {
+    this.loginService.create(this.apiUrlLogin, credentials).subscribe(
+      x => {
+        if (x.accessToken) {
+          sessionStorage.setItem("JWT", x.accessToken);
+          this.isLoggedInSubject.next(true);
+        } else {
+          console.log("login failed")
+        }
+      },
+      error => {
+        console.log("login failed")
+        this.isLoggedInSubject.next(false);
+      }
+    )
+  }
+
+  isLoggedIn(): void {
+    this.http.get(this.apiUrlSecured).subscribe(response => {
+      console.log("is logged in")
+      this.isLoggedInSubject.next(true);
+    }, error => {
+      console.log("not logged in")
+      this.isLoggedInSubject.next(false);
+    })
+  }
+
+}
