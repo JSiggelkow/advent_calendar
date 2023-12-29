79d03c707caa178f06d33625c26a417e1eb6344f
JSiggelkow <J.Siggelkow@iks-gmbh.com>
2023-12-29 12:09:57 +0100

    🚨 fixed compiler warnings - unused inputs

--- advent_calendar_backend/src/main/java/de/iks/advent_calendar_backend/rest/service/UserService.java
+++ advent_calendar_backend/src/main/java/de/iks/advent_calendar_backend/rest/service/UserService.java
@@ -3,9 +3,7 @@ package de.iks.advent_calendar_backend.rest.service;
 import de.iks.advent_calendar_backend.dao.UserRepo;
 import de.iks.advent_calendar_backend.entity.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
